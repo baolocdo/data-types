1459829a0a4a3463429dfbe82206aafc7a0dca5c
Alex Do <baolocdo@users.noreply.github.com>
2021-02-07 20:44:01 -0800

    Add files via upload

---
+++ variable.js
@@ -0,0 +1,17 @@
+// TODO: Create a variable named `firstName` and assign your first name to it in the line below.
+let firstName = undefined;
+
+// TODO: Create a variable named `lastName` and assign your last name to it in the line below.
+
+
+// TODO: Create a variable named `sayHello` and build the following string based on the variables
+// above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!". 
+
+
+// DO NOT CHANGE ANYTHING BELOW THIS LINE.
+
+module.exports = {
+  firstName,
+  lastName,
+  sayHello
+};
