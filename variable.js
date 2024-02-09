9578d73ed022f457de192ec26224f20f329e8340
hnguyen1537 <159486736+hnguyen1537@users.noreply.github.com>
2024-02-08 16:20:41 -0800

    Update variable.js

--- variable.js
+++ variable.js
@@ -1,11 +1,14 @@
 // TODO: Create a variable named `firstName` and assign your first name to it in the line below.
 let firstName = undefined;
 
+let firstName = 'Trinh';
 // TODO: Create a variable named `lastName` and assign your last name to it in the line below.
-
+let lastName = 'Nguyen'; 
 
 // TODO: Create a variable named `sayHello` and build the following string based on the variables
 // above: "Hello world! My name is <firstName> <lastName>. Nice to meet you!". 
+
+let sayHello = 'Hello World! My name is ${firstName} ${lastName}. Nice to meet you!".;
 
 
 // DO NOT CHANGE ANYTHING BELOW THIS LINE.
